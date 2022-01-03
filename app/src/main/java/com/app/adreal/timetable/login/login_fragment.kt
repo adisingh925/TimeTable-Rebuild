34e6bfbf67629407d95ac63e6e5285bc04513519
aditya singh <adisingh925@gmail.com>
2022-01-03 11:22:53 +0530

    auth_fragment_creation

---
+++ app/src/main/java/com/app/adreal/timetable/login/login_fragment.kt
@@ -0,0 +1,20 @@
+package com.app.adreal.timetable.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.adreal.timetable.R
+
+
+class login_fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_fragment, container, false)
+    }
+}
