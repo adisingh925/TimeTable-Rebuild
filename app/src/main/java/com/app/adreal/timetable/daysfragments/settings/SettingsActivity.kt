3d0c12fb1429f1fa4e3a43542f0b7b484e7f5f25
aditya singh <adisingh925@gmail.com>
2022-01-29 14:06:57 +0530

    removed most of boilerplate code

--- app/src/main/java/com/app/adreal/timetable/daysfragments/settings/SettingsActivity.kt
+++ app/src/main/java/com/app/adreal/timetable/daysfragments/settings/SettingsActivity.kt
@@ -36,10 +36,7 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == android.R.id.home)
-        {
-
-        }
+        finish()
         return super.onOptionsItemSelected(item)
     }
 
